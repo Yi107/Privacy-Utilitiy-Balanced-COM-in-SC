8ec25540f74a93fe8024c2f63db0dc9d042827b6
Yi107 <102788254+Yi107@users.noreply.github.com>
2024-08-28 19:16:25 +0800

    Update estimatePrice.h

--- Code/estimatePrice.h
+++ Code/estimatePrice.h
@@ -3,5 +3,4 @@
 void caltotMin(map<int, float> *minAvg, float distance, int *mintype, float *minpri);
 void pri_calMinForEvery(map<int, multiset<Worker>> *avaWorker, map<int, float> *minAvg, float priepsilon);
 void calMinForEvery(map<int, multiset<Worker>> *avaWorker, map<int, float> *minAvg);
-//void calAverForW(map<int, multiset<Worker>> *avaWorker, map<int, float> *avgForEP);
 void calAvgMin(map<int, float> *avgForEP, int *mintype, float *minpri);
